68c560e495b1bd408bed364fad8b123e8d06ba47
Joseph Hodes <jojoho70@gmail.com>
2021-05-30 20:45:54 -0400

    Post is working with basic paramaters but only accepts encoded forma data

--- Main.go
+++ Main.go
@@ -5,13 +5,33 @@ import (
 	"net/http"
 )
 
-func auth() {
+//note it only accepts encoded form data
+func auth(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		username := r.FormValue("username")
+		password := r.FormValue("password")
 
+		if username == "" || password == "" {
+			w.WriteHeader(404)
+		} else {
+			w.WriteHeader(200)
+			fmt.Println(username)
+			fmt.Println(password)
+		}
+
+	}
 }
-func handlerequests(w *http.ResponseWriter, r *http.Request) {
+
+func handleRequests() {
 	http.HandleFunc("/", auth)
+	http.ListenAndServe(":8000", nil)
 }
 func main() {
 
-	handlerequests()
+	handleRequests()
 }
