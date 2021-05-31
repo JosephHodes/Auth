a1edad9235405aef1c777213810d1b1d60c6c6c3
Joseph Hodes <jojoho70@gmail.com>
2021-05-30 23:53:41 -0400

    allows json data

--- Main.go
+++ Main.go
@@ -1,27 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
+
+type authentication struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 //note it only accepts encoded form data
 func auth(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
-			return
+		auth := authentication{}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), 400)
 		}
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-
-		if username == "" || password == "" {
-			w.WriteHeader(404)
+		err = json.Unmarshal(body, &auth)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), 400)
+		}
+		if auth.Username == "" || auth.Password == "" {
+			w.WriteHeader(401)
 		} else {
 			w.WriteHeader(200)
-			fmt.Println(username)
-			fmt.Println(password)
+
+			log.Println(auth.Username)
+			log.Println(auth.Password)
 		}
 
 	}
